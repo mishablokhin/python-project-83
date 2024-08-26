267d7130cdd6ad3a3fed14d5f5bfc5799986577f
mishablokhin <mishablokhin@yandex.ru>
2024-08-26 16:14:09 +0300

    Added separate python file to handle db logic

---
+++ page_analyzer/db.py
@@ -0,0 +1,16 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+def open_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
+
+
+def close_connection(conn):
+    conn.close()
