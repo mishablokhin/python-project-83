08166daf4db34f0988529c98adc2b2c21f2c6721
mishablokhin <mishablokhin@yandex.ru>
2024-08-28 23:34:11 +0300

    Moved all database logic to another file

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,6 +1,8 @@
 import os
 import psycopg2
+from psycopg2.extras import NamedTupleCursor
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -14,3 +16,40 @@ def open_connection():
 
 def close_connection(conn):
     conn.close()
+
+
+def commit_changes(conn):
+    conn.commit()
+
+
+def is_url_already_exists(conn, url):
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT id FROM urls WHERE name = %s", (url,))
+        return cursor.fetchone() is not None
+
+
+def get_all_urls(conn):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute("SELECT * FROM urls ORDER BY created_at DESC")
+        return cursor.fetchall()
+
+
+def add_new_url_to_db(conn, url):
+    with conn.cursor() as cursor:
+        current_date = datetime.now().strftime('%Y-%m-%d')
+        cursor.execute(
+            "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id",
+            (url, current_date)
+        )
+        new_id = cursor.fetchone()[0]
+        commit_changes(conn)
+        return new_id
+
+
+def get_url_info_by_id(conn, id):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute(
+            "SELECT * FROM urls WHERE id = %s",
+            (id)
+        )
+        return cursor.fetchone()
