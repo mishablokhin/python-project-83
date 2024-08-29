e8f8e7498e94f8fffb21af2d0b073221ad4e6509
mishablokhin <mishablokhin@yandex.ru>
2024-08-29 22:30:30 +0300

    Added sql to create table url_checks

--- database.sql
+++ database.sql
@@ -4,3 +4,14 @@ CREATE TABLE urls (
     name VARCHAR(255) NOT NULL,
     created_at DATE NOT NULL
 );
+
+DROP TABLE IF EXISTS url_checks;
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id integer NOT NULL,
+    status_code integer NOT NULL,
+    h1 text,
+    title text,
+    description text,
+    created_at DATE NOT NULL
+);
