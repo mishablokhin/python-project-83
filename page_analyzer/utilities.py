0bee0b175b197248505ecec67dccf962e94afee1
mishablokhin <mishablokhin@yandex.ru>
2024-08-30 23:36:29 +0300

    Added function to make request to url

--- page_analyzer/utilities.py
+++ page_analyzer/utilities.py
@@ -1,5 +1,6 @@
 from validators import url, ValidationError
 from urllib.parse import urlparse, urlunparse
+import requests
 
 
 def is_valid_url(url_to_validate):
@@ -16,3 +17,14 @@ def normalize_url(url_to_normalize):
     normalized_url = urlunparse((parsed_url.scheme,
                                  parsed_url.netloc, '', '', '', ''))
     return normalized_url
+
+
+def make_request_to_url(url):
+    try:
+        site_request = requests.get(url)
+        site_request.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        return err
+    else:
+        if site_request.status_code == 200:
+            return 200
