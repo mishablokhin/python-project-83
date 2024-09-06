fa8cb8a308b3bd271346324afe64f7b60e44258a
mishablokhin <mishablokhin@yandex.ru>
2024-09-06 16:30:04 +0300

    Added unique and secondary key

--- database.sql
+++ database.sql
@@ -1,17 +1,21 @@
 DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
     created_at DATE NOT NULL
 );
 
 DROP TABLE IF EXISTS url_checks;
 CREATE TABLE url_checks (
     id SERIAL PRIMARY KEY,
-    url_id integer NOT NULL,
-    status_code integer,
-    h1 text,
-    title text,
-    description text,
-    created_at DATE NOT NULL
+    url_id INTEGER NOT NULL,
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
+    created_at DATE NOT NULL,
+    CONSTRAINT fk_url
+        FOREIGN KEY(url_id) 
+        REFERENCES urls(id)
+        ON DELETE CASCADE
 );
