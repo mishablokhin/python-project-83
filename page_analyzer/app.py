08e55aa79a47e3ab2d0f91797680be27afb53387
mishablokhin <mishablokhin@yandex.ru>
2024-09-06 20:59:38 +0300

    All database connection logic now at db.py, removed conn usage from app.py while function call

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -10,7 +10,6 @@ from flask import (
     flash
 )
 from .db import (
-    get_db_connection,
     add_new_url_to_db,
     get_all_urls,
     get_url_info_by_id,
@@ -42,18 +41,16 @@ def show_main_page():
 # Страница со всеми добавленными в БД адресами сайтов
 @app.get('/urls')
 def show_added_urls_page():
-    with get_db_connection() as conn:
-        urls = get_all_urls(conn)
-        urls_latest_check = get_latest_url_check(conn)
+    urls = get_all_urls()
+    urls_latest_check = get_latest_url_check()
     return render_template('urls.html', urls=urls, checks=urls_latest_check)
 
 
 # Страница с подробностями о конкретном адресе
 @app.get('/urls/<id>')
 def show_url_info(id):
-    with get_db_connection() as conn:
-        url_info = get_url_info_by_id(conn, id)
-        url_checks_info = get_url_checks_by_id(conn, id)
+    url_info = get_url_info_by_id(id)
+    url_checks_info = get_url_checks_by_id(id)
     return render_template('url.html',
                            url_info=url_info, checks=url_checks_info)
 
@@ -65,17 +62,16 @@ def add_new_url():
     try:
         if is_valid_url(url):
             normalized_url = normalize_url(url)
-            with get_db_connection() as conn:
-                existing_url = get_url_id_if_exists(conn, normalized_url)
-                if existing_url:
-                    existing_url_id = existing_url[0]
-                    flash('Страница уже существует', 'alert-info')
-                    return redirect(url_for('show_url_info',
-                                            id=existing_url_id))
-                else:
-                    added_url_id = add_new_url_to_db(conn, normalized_url)
-                    flash('Страница успешно добавлена', 'alert-success')
-                    return redirect(url_for('show_url_info', id=added_url_id))
+            existing_url = get_url_id_if_exists(normalized_url)
+            if existing_url:
+                existing_url_id = existing_url[0]
+                flash('Страница уже существует', 'alert-info')
+                return redirect(url_for('show_url_info',
+                                        id=existing_url_id))
+            else:
+                added_url_id = add_new_url_to_db(normalized_url)
+                flash('Страница успешно добавлена', 'alert-success')
+                return redirect(url_for('show_url_info', id=added_url_id))
         else:
             flash('Некорректный URL', 'alert-danger')
             return render_template('index.html'), 422
@@ -87,22 +83,18 @@ def add_new_url():
 # Обработчик формы запуска проверки для сайта
 @app.post('/urls/<id>/check')
 def check_url(id):
-    with get_db_connection() as conn:
-        url_name = get_url_name_by_id(conn, id)
-        try:
-            response = requests.get(url_name)
-            response.raise_for_status()
+    url_name = get_url_name_by_id(id)
+    try:
+        response = requests.get(url_name)
+        response.raise_for_status()
 
-            html_content = response.text
+        html_content = response.text
+        site_data = get_seo_information_from_page(html_content)
 
-            site_data = get_seo_information_from_page(html_content)
-            h1, title, description = site_data['h1'], \
-                site_data['title'], \
-                site_data['description']
+        h1, title, description = site_data['h1'], site_data['title'], site_data['description']
 
-            add_url_check(conn, id, h1, title, description)
-            flash('Страница успешно проверена', 'alert-success')
-        except requests.exceptions.RequestException as err:
-            flash('Произошла ошибка при проверке: {}'.format(err),
-                  'alert-danger')
+        add_url_check(id, h1, title, description)
+        flash('Страница успешно проверена', 'alert-success')
+    except requests.exceptions.RequestException as err:
+        flash('Произошла ошибка при проверке: {}'.format(err), 'alert-danger')
     return redirect(url_for('show_url_info', id=id))
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -20,83 +20,84 @@ def get_db_connection():
         conn.close()
 
 
-def get_url_id_if_exists(conn, url):
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute("SELECT id FROM urls WHERE name = %s", (url,))
-        return cursor.fetchone()  # Возвращаем саму запись, если существует
+def get_url_id_if_exists(url):
+    with get_db_connection() as conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute("SELECT id FROM urls WHERE name = %s", (url,))
+            return cursor.fetchone()
 
 
-def get_all_urls(conn):
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute("SELECT * FROM urls ORDER BY created_at DESC")
-        return cursor.fetchall()
+def get_all_urls():
+    with get_db_connection() as conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute("SELECT * FROM urls ORDER BY created_at DESC")
+            return cursor.fetchall()
 
 
-def add_new_url_to_db(conn, url):
-    with conn.cursor() as cursor:
-        current_date = datetime.now().strftime('%Y-%m-%d')
-        cursor.execute(
-            "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id",
-            (url, current_date)
-        )
-        new_id = cursor.fetchone()[0]
-        conn.commit()
-        return new_id
-
-
-def get_url_info_by_id(conn, id):
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute(
-            "SELECT * FROM urls WHERE id = %s",
-            (int(id),)
-        )
-        return cursor.fetchone()
-
-
-def get_url_checks_by_id(conn, id):
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute(
-            """
-            SELECT * FROM url_checks
-            WHERE url_id = %s
-            ORDER BY created_at DESC, id DESC
-            """,
-            (id,)
-        )
-        return cursor.fetchall()
-
-
-def add_url_check(conn, id, h1, title, description):
+def add_new_url_to_db(url):
     current_date = datetime.now().strftime('%Y-%m-%d')
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """
-            INSERT INTO url_checks
-            (url_id, status_code, h1, title, description, created_at)
-            VALUES (%s, %s, %s, %s, %s, %s)
-            """,
-            (id, 200, h1, title, description, current_date)
-        )
-        conn.commit()
+    with get_db_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id",
+                (url, current_date)
+            )
+            new_id = cursor.fetchone()[0]
+            conn.commit()
+            return new_id
 
 
-def get_url_name_by_id(conn, id):
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute(
-            "SELECT name FROM urls WHERE id = %s",
-            (id,)
-        )
-        return cursor.fetchone()[0]
+def get_url_info_by_id(id):
+    with get_db_connection() as conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute("SELECT * FROM urls WHERE id = %s", (int(id),))
+            return cursor.fetchone()
 
 
+def get_url_checks_by_id(id):
+    with get_db_connection() as conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute(
+                """
+                SELECT * FROM url_checks
+                WHERE url_id = %s
+                ORDER BY created_at DESC, id DESC
+                """,
+                (id,)
+            )
+            return cursor.fetchall()
 
-def get_latest_url_check(conn):
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute(
-            """
-            SELECT DISTINCT ON (url_id) url_id, status_code, created_at
-            FROM url_checks
-            ORDER BY url_id, created_at DESC;
-            """
-        )
-        return cursor.fetchall()
+
+def add_url_check(id, h1, title, description):
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    with get_db_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                INSERT INTO url_checks
+                (url_id, status_code, h1, title, description, created_at)
+                VALUES (%s, %s, %s, %s, %s, %s)
+                """,
+                (id, 200, h1, title, description, current_date)
+            )
+            conn.commit()
+
+
+def get_url_name_by_id(id):
+    with get_db_connection() as conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute("SELECT name FROM urls WHERE id = %s", (id,))
+            return cursor.fetchone()[0]
+
+
+def get_latest_url_check():
+    with get_db_connection() as conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute(
+                """
+                SELECT DISTINCT ON (url_id) url_id, status_code, created_at
+                FROM url_checks
+                ORDER BY url_id, created_at DESC;
+                """
+            )
+            return cursor.fetchall()
